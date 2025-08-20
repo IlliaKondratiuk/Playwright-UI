4d01235237351d80fdd907b79a405e75d3a03740
Illia <qa.kond.work@gmail.com>
2025-08-20 11:36:15 +0200

    Add: urls.ts

---
+++ config/urls.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://ultimateqa.com';
+
+export const PAGES = {
+    COMPLICATED: `${BASE_URL}/complicated-page`,
+    LANDING: `${BASE_URL}/fake-landing-page`,
+    PRICING: `${BASE_URL}/automation/fake-pricing-page`,
+    FORMS: `${BASE_URL}/filling-out-forms`,
+    LIFECYCLE: `${BASE_URL}/sample-application-lifecycle-sprint-1`, 
+    SIMPLE_ELEMENTS: `${BASE_URL}/simple-html-elements-for-automation`
+}
