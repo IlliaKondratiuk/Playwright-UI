4859c60e7f9623e7f23aebe6b7f8ab404e760f3f
Illia <qa.kond.work@gmail.com>
2025-08-22 12:17:59 +0200

    Add: locators for courses in LandingPage.ts

--- pages/LandingPage.ts
+++ pages/LandingPage.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 import { PAGES } from "../config/urls";
 import { BasePage } from "./BasePage";
 
@@ -6,8 +6,16 @@ export class LandingPage extends BasePage {
 
     url = PAGES.LANDING;
 
+    readonly learningBlocks: Locator;
+
     constructor(page: Page) {
         super(page);
+        this.learningBlocks = page.locator("div[class='et_pb_row'] h4.et_pb_module_header");
+    }
+
+    async getLearningBlocksText(): Promise<string[]> {
+        const blocks = await this.learningBlocks.allTextContents();
+        return blocks.map(block => block.trim());
     }
 
 }
