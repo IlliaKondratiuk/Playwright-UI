c127645b9643924be83ebbe8de15914d4e4ee298
Illia <qa.kond.work@gmail.com>
2025-08-22 12:28:19 +0200

    Add: course blocks test to LandingTest.specs.ts

--- tests/LandingTest.spec.ts
+++ tests/LandingTest.spec.ts
@@ -1,9 +1,20 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { LandingPage } from "../pages/LandingPage";
+import { COURSES } from "../test_data/courses";
 
 test("open page, wait 5s, and close", async ({ page }) => {
   const landingPage = new LandingPage(page);
     await landingPage.goto(landingPage.url);
     await landingPage.waitForSeconds(5);
-  // Browser will close automatically after test ends
+  
+});
+
+test("course blocks contain expected text", async ({ page }) => { 
+
+  const landingPage = new LandingPage(page);
+
+  await landingPage.goto(landingPage.url);
+
+  expect.soft(await landingPage.getLearningBlocksText()).toEqual(COURSES);
+
 });
