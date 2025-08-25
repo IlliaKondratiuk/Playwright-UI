b021ef024529004f0b5189b169fea6af99fb28a1
Illia <qa.kond.work@gmail.com>
2025-08-25 14:00:30 +0200

    Add: Prettier module and configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, prettier, {
+  languageOptions: {
+    parser: tseslint.parser,
+  },
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+  },
+});
