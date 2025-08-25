65259c4c3fa991d1e4f15ff230b736613f24a402
Illia <qa.kond.work@gmail.com>
2025-08-25 13:35:13 +0200

    Add: ComplicatedPage.ts

---
+++ pages/ComplicatedPage.ts
@@ -0,0 +1,34 @@
+import { Page, Locator } from "@playwright/test";
+import { PAGES } from "../config/urls";
+import { BasePage } from "./BasePage";
+
+export class ComplicatedPage extends BasePage {
+
+    url = PAGES.COMPLICATED;
+
+    readonly firstSectionButtons: Locator;
+    readonly twitterButtons: Locator;
+    readonly facebookButtons: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.firstSectionButtons = page.locator("a[class*='et_pb_button']");
+        this.twitterButtons = page.locator("li[class*='social_media'] > a[title*='Twitter']");
+        this.facebookButtons = page.locator("li[class*='social_media'] > a[title*='Facebook']");
+    }
+
+    async clickEverySecondButton(): Promise<void> {
+        const count = await this.firstSectionButtons.count();
+        for (let i = 1; i < count; i += 2) {
+            await this.click(this.firstSectionButtons.nth(i));
+        }
+    }
+
+    async clickFirstTwitterButton(): Promise<void> {
+        await this.click(this.twitterButtons.first());
+    }
+
+    async clickFirstFacebookButton(): Promise<void> {
+        await this.click(this.facebookButtons.first());
+    }
+}
