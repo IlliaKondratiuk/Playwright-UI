fac9503304eb91a39754914a75aa493a9ce6e731
Illia <qa.kond.work@gmail.com>
2025-09-03 15:34:03 +0200

    Fix: compiler module for imports

--- tests/ComplicatedTest.spec.ts
+++ tests/ComplicatedTest.spec.ts
@@ -1,6 +1,7 @@
 //This class contains tests for the "Complicated" page
 import { expect, test } from "@playwright/test";
 import { ComplicatedPage } from "../pages/ComplicatedPage";
+import testData from "../test_data/general_data.json" assert { type: "json" };;
 
 test("click every second button in first section", async ({ page }) => {
   const complicatedPage = new ComplicatedPage(page);
@@ -19,4 +20,16 @@ test("check social media links", async ({ page }) => {
   await page.goBack();
   await complicatedPage.clickFirstFacebookButton();
   await expect(page).toHaveURL(/facebook.com/, { timeout: 5000 });
+});
+
+test("fill and submit form", async ({ page }) => {
+  const complicatedPage = new ComplicatedPage(page);
+
+  const formName = testData.complicated_form.formName;
+  const formEmail = testData.complicated_form.formEmail;
+  const formMessage = testData.complicated_form.formMessage;
+
+  await complicatedPage.goto(complicatedPage.url);
+
+  await complicatedPage.fillAndSubmitForm(formName, formEmail, formMessage);
 });
