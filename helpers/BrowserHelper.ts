485bc8be237b58f4ad90b071d68dd21656813668
Illia <qa.kond.work@gmail.com>
2025-09-10 10:37:55 +0200

    Add: BrowserHelper.ts

---
+++ helpers/BrowserHelper.ts
@@ -0,0 +1,13 @@
+import { Page } from "@playwright/test";
+
+export class BrowserHelper {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async isAtTop(): Promise<boolean> {
+        return this.page.evaluate(() => window.scrollY === 0);
+    }
+}
