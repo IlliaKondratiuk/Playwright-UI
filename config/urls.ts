dfd8b7803b41c5a57e6aad35bc80ab808cc2342a
Illia <qa.kond.work@gmail.com>
2025-09-19 14:05:58 +0200

    Remove: unused URL from urls.ts

--- config/urls.ts
+++ config/urls.ts
@@ -4,7 +4,6 @@ export const PAGES = {
   COMPLICATED: `${BASE_URL}/complicated-page`,
   LANDING: `${BASE_URL}/fake-landing-page`,
   PRICING: `${BASE_URL}/automation/fake-pricing-page`,
-  FORMS: `${BASE_URL}/filling-out-forms`,
   LIFECYCLE: `${BASE_URL}/sample-application-lifecycle-sprint-1`,
   SIMPLE_ELEMENTS: `${BASE_URL}/simple-html-elements-for-automation`,
 };
