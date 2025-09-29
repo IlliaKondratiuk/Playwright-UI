274c44835d1fe0d2e8320e4c662a4f0e7315eecd
Illia <qa.kond.work@gmail.com>
2025-09-29 13:05:35 +0200

    Add: pretest enviroment generation for Allure reporting

---
+++ scripts/AllureEnvGenerator.ts
@@ -0,0 +1,35 @@
+import {  BrowserType, chromium, firefox, webkit } from 'playwright';
+import fs from 'fs';
+import path from 'path';
+
+async function generateAllureEnv() {
+  // Detect browser & version based on environment variable PW_BROWSER
+  const browserName = process.env.PLAYWRIGHT_BROWSER || 'chromium'; // fallback
+    const launchers: Record<string, BrowserType> = {
+    chromium,
+    firefox,
+    webkit,
+  };
+
+  const browser = await launchers[browserName].launch();
+  const version = browser.version();
+  await browser.close();
+
+  // System details
+  const os = `${process.platform} ${process.arch}`;
+  const baseUrl = process.env.BASE_URL || "http://localhost";
+
+  const envContent = `
+BROWSER=${browserName} ${version}
+OS=${os}
+BASE_URL=${baseUrl}
+`;
+
+  const resultsDir = path.join('allure-results');
+  if (!fs.existsSync(resultsDir)) fs.mkdirSync(resultsDir);
+
+  fs.writeFileSync(path.join(resultsDir, 'environment.properties'), envContent.trim());
+  console.log("✅ Allure environment.properties generated:\n", envContent);
+}
+
+generateAllureEnv();
